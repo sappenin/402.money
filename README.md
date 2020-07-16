c39929ef226695b18d2432939e8d7fc5bef6e30c
sappenin <sappenin@gmail.com>
2020-07-16 17:18:16 -0600

    Simulate redirects
    Signed-off-by: sappenin <sappenin@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-<meta name="monetization" content="$coil.xrptipbot.com/LbppTUw0QpWt7fvEzaDHEw">
-
 This is the placeholder website for https://402.money.
---
+++ redirect.js
@@ -0,0 +1,14 @@
+// This attempts to emulate a 301 redirect behavior for users in a browser, but won't actually work for PayID Clients. :(
+
+var redirects = {
+    '?resource=payid:david$402.money': 'https://payid.402.money/users/david',
+    '?resource=payid:alice$402.money': 'https://payid.402.money/users/alice',
+    '?resource=payid:pay$402.money': 'https://payid.402.money/pay',
+};
+
+(function() {
+    var s = document.location.search;
+    if (s in redirects) {
+        window.location.replace('/wp/' + redirects[s]);
+    }
+})();
